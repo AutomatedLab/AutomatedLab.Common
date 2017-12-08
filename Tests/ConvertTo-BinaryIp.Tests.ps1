379a70e0f0a6b30234ddcc7282642bed698d7d47
Jan-Hendrik Peters [MSFT] <nyanhp@users.noreply.github.com>
2017-12-08 11:15:56 +0100

    Test added for ConvertTo-BinaryIp

--- Tests/ConvertTo-BinaryIp.Tests.ps1
+++ Tests/ConvertTo-BinaryIp.Tests.ps1
@@ -5,3 +5,27 @@ if (-not $ENV:BHProjectPath)
 
 Remove-Module $ENV:BHProjectName -ErrorAction SilentlyContinue -Force
 Import-Module (Join-Path $ENV:BHProjectPath $ENV:BHProjectName) -Force
+
+InModuleScope -ModuleName $ENV:BHProjectName {
+    Describe "ConvertTo-BinaryIp" {
+        $goodIp = "192.168.2.1"
+        $badIp = "555.123.123.123"
+
+        Context "Valid IP" {
+            
+            It "Should return a binary dotted IP" {
+                ConvertTo-BinaryIP -IPAddress $goodIp | Should BeExactly "11000000.10101000.00000010.00000001"
+            }
+
+            It "Should not throw" {
+                {ConvertTo-BinaryIP -IPAddress $goodIp} | Should Not Throw
+            }
+        }
+
+        Context "Invalid IP" {
+            It "Should throw" {
+                {ConvertTo-BinaryIP -IPAddress $bad} | Should Throw
+            }
+        }
+    }
+}
