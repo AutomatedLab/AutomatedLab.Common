9e00248f77eb2c362160fbdc12cf53abe6de56e9
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2019-03-29 14:27:55 +0100

    All cmdlets tested and fixed or exception added

--- AutomatedLab.Common/Common/Public/Install-SoftwarePackage.ps1
+++ AutomatedLab.Common/Common/Public/Install-SoftwarePackage.ps1
@@ -14,35 +14,7 @@ function Install-SoftwarePackage
         [int[]]$ExpectedReturnCodes,
 
         [system.management.automation.pscredential]$Credential
-    )
-
-    Add-Type -TypeDefinition @'
-namespace AutomatedLab.Common
-{
-    using System;
-
-    public class Win32Exception : System.ComponentModel.Win32Exception
-    {
-        public new int ErrorCode { get; set; }
-
-        public Win32Exception(int error) : base(error)
-        {
-            ErrorCode = error;
-        }
-
-        public Win32Exception(string message) : base(message)
-        { }
-
-        public Win32Exception(int error, string message)  : base(error, message)
-        {
-            ErrorCode = error;
-        }
-
-        public Win32Exception(string message , Exception innerException) : base(message, innerException)
-        { }
-    }
-}
-'@
+    )    
     
     #region New-InstallProcess
     function New-InstallProcess
