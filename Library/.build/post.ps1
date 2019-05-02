8cb4450c7990483bea630717b2ad50ae7a8d5084
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2019-05-02 15:40:00 +0200

    moved things around

--- Library/.build/post.ps1
+++ Library/.build/post.ps1
@@ -3,8 +3,9 @@ param
 	[string]
 	$SolutionDir
 )
+
 $path = Join-Path -Path $SolutionDir -ChildPath Library
-$destination = Join-Path -Path (Resolve-Path $SolutionDir\..).Path -ChildPath AutomatedLab.Common
+$destination = Join-Path -Path $SolutionDir -ChildPath AutomatedLab.Common
 
 $coreClr = Get-ChildItem -Recurse -Filter *.dll -Path $path | Where {$_.FullName -match 'coreapp' }
 $fullClr = Get-ChildItem -Recurse -Filter *.dll -Path $path | Where {$_.FullName -notmatch 'coreapp|standard' }
