ac54d0b1f0fa20e1b8a9e9b9ae22c0959e9d59b3
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2019-05-02 16:04:26 +0200

    Now with path...

--- Library/.build/post.ps1
+++ Library/.build/post.ps1
@@ -10,14 +10,14 @@ $destination = Join-Path -Path $SolutionDir -ChildPath AutomatedLab.Common
 $coreClr = Get-ChildItem -Recurse -Filter *.dll -Path $path | Where {$_.FullName -match 'coreapp' }
 $fullClr = Get-ChildItem -Recurse -Filter *.dll -Path $path | Where {$_.FullName -notmatch 'coreapp|standard' }
 
-if (-not (Test-Path ))
+if (-not (Test-Path $destination\lib\core))
 {
-	$null = mkdir $destination\lib\core
+	$null = mkdir $destination\lib\core -Force
 }
 
-if (-not (Test-Path))
+if (-not (Test-Path $destination\lib\full))
 {
-	$null = mkdir $destination\lib\core
+	$null = mkdir $destination\lib\full-Force
 }
 
 $coreClr | Copy-Item -Destination $destination\lib\core
